dfb7626e8263209197adfa9f7206aa4275167a5e
Your Name <ou@example.com>
2018-12-05 10:48:07 +0800

    complete

--- clock.js
+++ clock.js
@@ -1,19 +1,55 @@
-// create a timer that only goes up to 30 seconds
+// // create a timer that only goes up to 30 seconds
 
-var counter = 0; // set the timer counter to zero
+// var counter = 0; // set the timer counter to zero
 
-var interval = null; // create a variable to put the interval into
+// var interval = null; // create a variable to put the interval into
 
-//set the interval
-interval = setInterval( function(){
+// //set the interval
+// interval = setInterval( function(){
 
-    counter++; // increment the counter
-    
-    console.log( "counter: "+counter); // output the current counter "time"
-    
-    // if 30 seconds have passed, stop the counter
-    if( counter === 30 ){
-      clearInterval( counter );
+//     counter++; // increment the counter
+
+//     console.log( "counter: "+counter); // output the current counter "time"
+
+//     // if 30 seconds have passed, stop the counter
+//     if( counter === 30 ){
+//       clearInterval( counter );
+//     }
+
+// }, 1000);
+
+    var seconds = 0;
+    var minutes = 0;
+    var hours = 0;
+setInterval(function(){
+    seconds = seconds + 1;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes += 1;
+        }
+    if (minutes == 60) {
+        minutes = 0;
+        hours += 1;
+        }
+    if (hours == 12) {
+        hours = 0;
+        }
+    function display(quantity) {
+        var newDisplay;
+        if (quantity < 10) {
+        newDisplay = "0"+ quantity;
+        }
+        else {
+            newDisplay = quantity;
+        } return newDisplay;
     }
-    
-}, 1000);
+    secondsDisplay = display(seconds);
+    minutesDisplay = display(minutes);
+    hoursDisplay = display(hours);
+    document.querySelector("#display").textContent = ( hoursDisplay + ":" + minutesDisplay + ":" + secondsDisplay );
+    document.querySelector("#second").style.transform =  "rotate("+(secondsDisplay*6)+"deg)";
+    document.querySelector("#minute").style.transform =  "rotate("+(minutesDisplay*6)+"deg)";
+    document.querySelector("#hour").style.transform =  "rotate("+(hoursDisplay*30)+"deg)";
+},5);
+
+
